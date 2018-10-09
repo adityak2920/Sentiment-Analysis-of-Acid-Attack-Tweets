3629a15b0b6883cfdd3dab779353f00d6eac3322
Aditya Kumar <35501699+adityak2920@users.noreply.github.com>
2018-10-09 15:06:16 +0530

    Update tweet_scrape.py

--- Assignment2/src/tweet_scrape.py
+++ Assignment2/src/tweet_scrape.py
@@ -40,12 +40,13 @@ for ix in range(200):
 f1.close()
 fo
 
-##the method which is coded above using selenium and bs4 is quite slow so it will take a lot of time to scrape around 20000 tweets it's performs fairly well for small no. of tweets. For this project i had used 'twint' github repo
+##the method which is coded above using selenium and bs4 is quite slow so it will take a lot of time to scrape around 
+##20000 tweets it's performs fairly well for small no. of tweets. For this project i had used 'twint' github repo
 ##Using twint i can scrape 10000 tweets in less than 15 minutes it's pretty faster than selenium method
 ##when using twint i don't need to write any code the steps for this are:
 ###1. clone the github repo of twint
 ###2. now open terminal and go to twint repo
-###3. then with one line of command i can scrape and save in csv format tweets so, the command for t tweets containg word 'acid attack threat' i just need to write:
-####  'python3 Twint.py -s 'acid attack threat' -o killacidindia.csv --csv'
-### so it's much more simple and it gives csv with tweets and all other information about tweets like user id, username, tweetid, retweets, links and much more.
-### and the other informations about the csv file is explained in preprocess.py
+###3. then with one line of command i can scrape and save in csv format tweets so, the command for the tweets containg word 
+###'acid attack threat' i just need to write: 'python3 Twint.py -s 'acid attack threat' -o killacidindia.csv --csv'
+## so it's much more simple and it gives csv with tweets and all other information about tweets like user id, username, 
+##tweetid, retweets, links and much more and the other informations about the csv file is explained in preprocess.py.
